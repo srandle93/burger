c45eeee721468b21004c80b2eb9215c42c61bfc7
srandle93 <randle.symone.l@gmail.com>
2018-10-13 16:54:39 -0500

    Adding CSS class/id

--- config/orm.js
+++ config/orm.js
@@ -20,7 +20,6 @@ const orm = {
 
   // selectAll()
   selectAll: (callback) => {
-
     // Run MySQL Query
     connection.query('SELECT * FROM burgers', (err, result) => {
       if (err) throw err;
@@ -31,58 +30,16 @@ const orm = {
 
   // insertOne()
   insertOne: (burger_name, callback) => {
-
-    // Create a new timestamp
-    // ----------------------------------------------------------
-    const d = new Date();
-    const timestamp = `${ d.getFullYear()}``${'_'}`; // must be string
-    const month = `${ d.getMonth()}` `${1}`// must be string
-      // handle 1 digit months
-      if(month.length == 1){
-        month = '0' + month;
-      }
-    timestamp += month `${'_'}`;
-    const day = `${ d.getDate()}`; // must be string
-      // handle 1 digit day of month
-      if(day.length == 1){
-        day = '0' + day;
-      }
-    timestamp += day + ' ';
-    const hour = '' + d.getHours(); // must be string
-      // handle 1 digit hour
-      if(hour.length == 1){
-        hour = '0' + hour;
-      }
-    timestamp += hour + ':';
-    const minute = '' + d.getMinutes(); // must be string
-      // handle 1 digit minute
-      if(minute.length == 1){
-        minute = '0' + minute;
-      }
-    timestamp += minute + ':';
-    const second = '' + d.getSeconds(); // must be string
-      // handle 1 digit second
-      if(second.length == 1){
-        second = '0' + second;
-      }
-    timestamp += second;
-    // ----------------------------------------------------------
-
     // Run MySQL Query
-    connection.query('INSERT INTO burgers SET ?', {
-      burger_name: burger_name,
-      devoured: false,
-      date: timestamp
-    }, function (err, result) {
+    connection.query('INSERT INTO burgers SET ?', [{burger_name: burger_name}, {devoured: false,}], (err, result) => {
       if (err) throw err;
       callback(result);
     });
 
-  }),
+  },
 
   // updateOne()
   updateOne: (burgerID, callback) => {
-
     // Run MySQL Query
     connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}], (err, result) => {
         if (err) throw err;
